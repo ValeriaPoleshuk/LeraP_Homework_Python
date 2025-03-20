0dfb34883cd24056fb8d1acaeeb82138eec84875
lera <t.valeria1986@gmail.com>
2025-03-20 16:04:13 +0300

    lesson 5

--- Lesson_05/Google_task_1.py
+++ Lesson_05/Google_task_1.py
@@ -1,19 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Chrome
 driver = webdriver.Chrome()
 
+# Клик по кнопке
 try:
     # Шаг 1: Открыть страницу
     driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
 
     # Шаг 2: Пять раз кликнуть на кнопку "Add Element"
     add_button = driver.find_element(By.XPATH, "//button[text()='Add Element']")
-    for _ in range(5):
+    for click_count in range(5):  # Используем понятное имя переменной
         add_button.click()
-        time.sleep(0.5)
+        # Ожидание, пока кнопка "Add Element" станет кликабельной
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Add Element']")))
 
     # Шаг 3: Собрать список кнопок "Delete"
     delete_buttons = driver.find_elements(By.XPATH, "//button[text()='Delete']")
